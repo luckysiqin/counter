56223ac01616e97dbafb9dc0d96b5024faf5dada
luckysiqin <a20131105804@Mac28.local>
2016-04-25 21:28:55 +0800

    change
    counter

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  counter
+//
+//  Created by 20131105804 on 16/4/25.
+//  Copyright © 2016年 计分器. All rights reserved.
+//
+
+import Foundation
--- ViewController.swift
+++ ViewController.swift
@@ -15,7 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
  
+    @IBOutlet weak var one: UIButton!
 
+    @IBAction func two(sender: AnyObject) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
